0528e2c72f07fbe09ebf31074c0621923440f8a9
mapologize <dedonoiizunfinity@gmail.com>
2022-09-24 18:12:50 +0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(""));
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "MorphNFTv2 API v1.0.0"
+  });
+});
+
+router.get('/getsignature/:pkey/:hash', (req, res) => {
+    const privateKey = req.params.pkey;
+    const hashmes = req.params.hash;
+    const sigObj = web3.eth.accounts.sign(hashmes, privateKey);
+    res.send(sigObj.signature);
+});
+
+app.use(`/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
