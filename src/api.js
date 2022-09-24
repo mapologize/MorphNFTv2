ba81e62756aee2f44bd8ca04271559989ff48d13
mapologize <dedonoiizunfinity@gmail.com>
2022-09-24 18:20:12 +0700

    first commit

--- src/api.js
+++ src/api.js
@@ -18,7 +18,7 @@ router.get('/getsignature/:pkey/:hash', (req, res) => {
     res.send(sigObj.signature);
 });
 
-app.use(`/api`, router);
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
