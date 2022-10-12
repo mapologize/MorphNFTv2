ac80e9b514e4a37225e8114f5ece8377e96adddf
mapologize <dedonoiizunfinity@gmail.com>
2022-10-12 19:56:38 +0700

    first commit

--- src/api.js
+++ src/api.js
@@ -25,7 +25,7 @@ router.get('/metadata/:tokenid', (req, res) => {
 });
 
 router.get('/morph/:tokenid', (req, res) => {
-  res.json(fetch('./jsondata/1/0.json'));
+  res.json('./jsondata/1/0.json');
 });
 
 app.use(`/.netlify/functions/api`, router);
